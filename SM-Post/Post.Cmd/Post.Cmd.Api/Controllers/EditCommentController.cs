d52b05c8f4049961a274c2465bc707373e566ce8
Sean Campbell <sean@campbelltech.co.za>
2022-01-21 17:19:50 +0200

    Completed implemenation of EditCommentController

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/EditCommentController.cs
@@ -0,0 +1,65 @@
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class EditCommentController : ControllerBase
+    {
+        private readonly ILogger<EditCommentController> _logger;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public EditCommentController(ILogger<EditCommentController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditMessageAsync(Guid id, EditCommentCommand command)
+        {
+            try
+            {
+                command.Id = id;
+                await _commandDispatcher.Send(command);
+
+                return Ok(new BaseResponse
+                {
+                    Message = "Edit comment request completed successfully!"
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.LogWarning(ex, "Client made a bad request.");
+
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.ToString()
+                });
+            }
+            catch (AggregateNotFoundException ex)
+            {
+                _logger.LogWarning(ex, "Could not retrieve aggregate, client passed an incorrect post ID targetting the aggregate.");
+
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.ToString()
+                });
+            }
+            catch (Exception ex)
+            {
+                var safeErrorMessage = $"Error while processing request to edit a comment with ID - {command.CommentId} that was made on a post with ID - {id}.";
+                _logger.LogError(ex, safeErrorMessage, id);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = safeErrorMessage
+                });
+            }
+        }
+    }
+}
