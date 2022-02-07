e21da951331f8cbc3a0a9f8c916426bcec4c73f4
Sean Campbell <sean@campbelltech.co.za>
2022-02-07 17:17:56 +0200

    Reverted to preferred logic

--- SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
@@ -22,7 +22,7 @@ namespace Post.Cmd.Infrastructure.Stores
         {
             var eventStream = await _eventStoreRepository.FindByAggregateId(aggregateId);
 
-            if (eventStream?.Any() != true)
+            if (eventStream == null || !eventStream.Any())
                 throw new AggregateNotFoundException("Incorrect post ID provided!");
 
             return eventStream.OrderBy(x => x.Version).Select(x => x.EventData).ToList();
